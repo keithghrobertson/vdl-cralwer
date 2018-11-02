e09d8c482c8de56153c6294e480eaa2c27d8ae32
Keith Robertson <keithrobertson@Keiths-MacBook-Pro-2.local>
2018-11-01 17:55:33 -0700

    added tiering calculations while removing all headers and blank text rows in table

--- index.js
+++ index.js
@@ -29,23 +29,50 @@ function scrapeHTML(body) {
   const parsedTable = $("table").parsetable()
 
   // Get the list of teams
-  const teams = parsedTable[0]
+  const teams = parsedTable[0];
 
-  // Remove first index cause it's the header
-  teams.shift()
+  //these positions in the array consist of titles and blank rows, so let's remove them
+  const removeValFromIndex = [0,13,14,27,28,41,42];    
+
+  for (var i = removeValFromIndex.length -1; i >= 0; i--){
+    teams.splice(removeValFromIndex[i],1);
+  }
 
   // Get list of score
   const scores = parsedTable[1]
+  
+  //remove the list of keys that are titles/blank
+  for (var i = removeValFromIndex.length -1; i >= 0; i--){
+    scores.splice(removeValFromIndex[i],1);
+  }
 
-  // Remove first index cause it's the header
-  scores.shift()
+  //TIER 1/A calculations. Each win = 8 points, so multiply number of wins by 8 to get their score.
+  var i;
+  for (i = 0; i < 12; i++) { 
+    scores[i] = scores[i] * 8;
+  }
+
+  //TIER 2/B calculations. Each win = 4 points, so multiply number of wins by 4 to get their score.
+  for (i = 12; i < 24; i++) { 
+    scores[i] = scores[i] * 4;
+  }
+
+  //TIER 3/C calculations. Each win = 2 points, so multiply number of wins by 2 to get their score.
+  for (i = 24; i < 36; i++) { 
+    scores[i] = scores[i] * 2;
+  }
+
+  //TIER 4/D calculations not needed. Each win = 1 point.
+
+  
+
 
   // This will be our object to plug data into
   let teamScores = {}
 
   // Make an object with team name as key, score as val
   for(let i in teams) {
-    teamScores[teams[i]] = scores[i]
+    teamScores[teams[i]] = parseInt(scores[i])
   }
 
   jsonfile.writeFile(`output/${url.split('.ca/')[1].replace(/\//g,'-')}.json`, teamScores)
